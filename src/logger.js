e81e6855527aa544e2e9eedcde0e7d60fbab3797
Couto <hello@luiscouto.pt>
2017-07-20 19:36:40 +0100

    style(NO-US): Fix prettier

--- src/logger.js
+++ src/logger.js
@@ -1,28 +1,28 @@
 /* eslint-disable no-console */
 
 function Logger(enableDebug) {
-    this.debugEnabled = enableDebug || false;
+  this.debugEnabled = enableDebug || false;
 }
 
-Logger.prototype.info = function () {
-    if (this.debugEnabled) {
-        var args = Array.prototype.slice.call(arguments);
-        console.info.apply(console, args);
-    }
+Logger.prototype.info = function() {
+  if (this.debugEnabled) {
+    var args = Array.prototype.slice.call(arguments);
+    console.info.apply(console, args);
+  }
 };
 
-Logger.prototype.debug = function () {
-    if (this.debugEnabled) {
-        var args = Array.prototype.slice.call(arguments);
-        console.debug.apply(console, args);
-    }
+Logger.prototype.debug = function() {
+  if (this.debugEnabled) {
+    var args = Array.prototype.slice.call(arguments);
+    console.debug.apply(console, args);
+  }
 };
 
-Logger.prototype.error = function () {
-    if (this.debugEnabled) {
-        var args = Array.prototype.slice.call(arguments);
-        console.error.apply(console, args);
-    }
+Logger.prototype.error = function() {
+  if (this.debugEnabled) {
+    var args = Array.prototype.slice.call(arguments);
+    console.error.apply(console, args);
+  }
 };
 
 export default Logger;
